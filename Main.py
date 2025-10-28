49cbfec6c40d768f241ce8272af21f08cde5e0cf
VincentSuwardy <vincentemmanuelsuwardy@gmail.com>
2025-10-28 16:39:42 +0700

    fix: README.md

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from Constraint import apply_constraints, fill_random, EMPTY, WHITE, BLACK
     size: 6 | 8 | 10 | 14 | 20
     diff: easy | hard
 '''
-PUZZLE_SIZE = "20"
+PUZZLE_SIZE = "6"
 PUZZLE_DIFF = "easy"
 
 '''
